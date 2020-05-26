4c798875bc44fd6da44b1d227dd68a2c3fbc5804
shruti0018 <65651136+shruti0018@users.noreply.github.com>
2020-05-26 12:17:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mental_health_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mental_health_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
